5f5e2af3f015132434df07d8e20335f51286e185
JannikMetz <156902066+JannikMetz@users.noreply.github.com>
2025-04-01 13:34:52 +0200

    Added Class Encode and Memory

---
+++ src/Encode.cs
@@ -0,0 +1,28 @@
+﻿namespace DefaultNamespace;
+
+//Class Encode is Readign the Code and Extracting the operations
+// and giving the operations to the Memory
+public class Encode
+{
+    
+using System.IO;
+
+public class FileReader
+{
+    public string ReadFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            return File.ReadAllText(filePath);
+        }
+        else
+        {
+            throw new FileNotFoundException("Die Datei wurde nicht gefunden.", filePath);
+        }
+    }
+    
+    
+    
+}
+    
+}
