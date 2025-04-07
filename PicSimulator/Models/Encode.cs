47f8d69cbe476e8721b6fe9756cbd0aff9f0deeb
Devin Schnurr <devinschnurr@gmail.com>
2025-04-07 14:01:02 +0200

    UI changes

--- PicSimulator/Models/Encode.cs
+++ PicSimulator/Models/Encode.cs
@@ -16,6 +16,7 @@ public class FileReader
 {
     public string ReadFile(string filePath)
     {
+        
         if (File.Exists(filePath))
         {
             return File.ReadAllText(filePath, Encoding.UTF8);
