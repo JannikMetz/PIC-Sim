b33071d3f15a974b062b71818ce8309e408575cc
JannikMetz <156902066+JannikMetz@users.noreply.github.com>
2025-04-14 15:54:02 +0200

    Added opcodes Array for Breakpoints and visualisation

--- PicSimulator/Models/Encode.cs
+++ PicSimulator/Models/Encode.cs
@@ -9,6 +9,8 @@ public class Encode
 {
 
     private Memory _memory;
+    
+    private int[] opcodeLines = new int[1024]; // Program memory (ROM) - 2kB
 
     public Encode(Memory memory)
     {
@@ -34,26 +36,37 @@ public class Encode
     {
         List<int> opcodes = new List<int>();
         string[] lines = input.Split('\n');
+        int ProgramCounterLineIndex = 0;
         
         Console.WriteLine("<----------- Extracting Opcodes ----------->");
         
         foreach (string line in lines)
         {
+            
             //check if line starts with whitespace or number
             if (!string.IsNullOrWhiteSpace(line) && line[0] != ' ')
             {
                 string[] parts = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
                 // Extract the opcode from the line
                 string code =  parts[1];
-                Console.WriteLine($"Opcode found at index {Array.IndexOf(lines, line)+1}: {code}");                
+                Console.WriteLine($"Opcode found at line index {Array.IndexOf(lines, line)+1}: {code}");
                 opcodes.Add(int.Parse(code, System.Globalization.NumberStyles.HexNumber));
+                
+                
+                // Save the opcode to the opcodeLines array
+
+                opcodeLines[ProgramCounterLineIndex] = Array.IndexOf(lines, line);
+                ProgramCounterLineIndex++;
             }
+
             
+
         }
         
         Console.WriteLine("<----------- End of Opcodes ----------->");
         
-        _memory.ProgramMemory = opcodes.ToArray();
+        // Write the opcodes to the program memory
+        _memory.ProgramMemory = opcodes.ToArray(); 
         
         
     }
